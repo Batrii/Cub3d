dba91a9c2a51019692e79eb9745ef10c9823fdb7
Batrii <dhj.nafiai@gmail.com>
2025-07-29 18:30:14 +0100

    get cub.h cleaned for norminette

--- cub.h
+++ cub.h
@@ -22,53 +22,51 @@
 
 typedef struct s_config
 {
-	char *no_texture;
-	char *so_texture;
-	char *we_texture;
-	char *ea_texture;
-	int floor_color_r;
-	int floor_color_g;
-	int floor_color_b;
-	int ceiling_color_r;
-	int ceiling_color_g;
-	int ceiling_color_b;
-	char **map;
-	int map_width;
-	int map_height;
-} t_config;
-
-
+	char	*no_texture;
+	char	*so_texture;
+	char	*we_texture;
+	char	*ea_texture;
+	int		floor_color_r;
+	int		floor_color_g;
+	int		floor_color_b;
+	int		ceiling_color_r;
+	int		ceiling_color_g;
+	int		ceiling_color_b;
+	char	**map;
+	int		map_width;
+	int		map_height;
+}	t_config;
 //get_next_line part
-char *get_next_line(int fd);
-char *my_strjoin(char *s1, char *s2);
-char *my_strdup(char *s);
-char *my_strchar(char *str, int c);
-size_t my_strlen(char *s);
+char	*get_next_line(int fd);
+char	*my_strjoin(char *s1, char *s2);
+char	*my_strdup(char *s);
+char	*my_strchar(char *str, int c);
+size_t	my_strlen(char *s);
 //helper functions
-int ft_strlen(const char *str);
-int ft_atoi(char *str);
-int check_range_rgb(int color);
-void free_split(char **split);
-int is_line_empty(char *line);
+int		ft_strlen(const char *str);
+int		ft_atoi(char *str);
+int		check_range_rgb(int color);
+void	free_split(char **split);
+int		is_line_empty(char *line);
 char	**utils_split(const char *string, char c);
-int all_six_config(t_config *config);
-int check_valid_colors(char **colors);
+int		all_six_config(t_config *config);
+int		check_valid_colors(char **colors);
 //extension check
-int check_extension(char *filename);
+int		check_extension(char *filename);
 //map check
-int once_player(char **map);
-int check_walls_top_bottom(char **map, int map_height);
-int check_walls_left_right(char **map, int map_height);
-int check_invalid_spaces(char **map, int map_height);
-int check_valid_characters(char **map, int map_height);
+int		once_player(char **map);
+int		check_walls_top_bottom(char **map, int map_height);
+int		check_walls_left_right(char **map, int map_height);
+int		check_invalid_spaces(char **map, int map_height);
+int		check_valid_characters(char **map, int map_height);
 //assign colors and textures
-int assign_texture(t_config *config, char *line);
-int assign_colors(t_config *config, char *line);
+int		assign_texture(t_config *config, char *line);
+int		assign_colors(t_config *config, char *line);
 //map creation
-int create_map(char *filename, t_config *config);
+int		create_map(char *filename, t_config *config);
 //check walls helpers
-int	check_right_walls(char **map, int len, int i);
-int	check_left_walls(char **map, int len, int i);
-int	check_bottom_walls(char **map, int map_height);
-int	check_top_walls(char **map, int map_height);
+int		check_right_walls(char **map, int len, int i);
+int		check_left_walls(char **map, int len, int i);
+int		check_bottom_walls(char **map, int map_height);
+int		check_top_walls(char **map, int map_height);
 #endif
